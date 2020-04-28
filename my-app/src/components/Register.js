d31a75d4718f6f6cc8bc26d2b201ee138dc29367
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-27 19:43:37 -0700

    have the styles for the register page and the basic form layed out.

--- my-app/src/components/Register.js
+++ my-app/src/components/Register.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import UserRegister from './Client/ClientRegister';
 import InstructorRegister from './Instructors/InstructorRegister';
-
+import Div from './styled-comp.jsx'
 
 function Register(props) {
 
 
     return (
-        <div>
+        <Div>
             <header>
                 <h3>Anywhere Fitness</h3>
             </header>
             <img src="/img/fitness-couple.jpg" className="registerImage" alt="Man holding woman up with his feet as she poses" />
-            
-        </div>
+            <h2>Sign Up</h2>
+            <div class="input-form">
+                <label>First Name:&nbsp;
+                        <input
+                            // value={values.firstname}
+                            // onChange={onInputChange}
+                            name='username'
+                            type='text'
+                        /></label>
+                <label>Last Name:&nbsp;
+                        <input
+                            // value={values.lastname}
+                            // onChange={onInputChange}
+                            name='username'
+                            type='text'
+                        /></label>
+                <label>Username:&nbsp;
+                        <input
+                            // value={values.username}
+                            // onChange={onInputChange}
+                            name='username'
+                            type='text'
+                        /></label>
+                    <label>Email:&nbsp;
+                        <input
+                            // value={values.email}
+                            // onChange={onInputChange}
+                            name='email'
+                            type='text'
+                        /></label>
+                    <label>Password:&nbsp;
+                        <input
+                            // value={values.password}
+                            // onChange={onInputChange}
+                            name='password'
+                            type='password'
+                        /></label>
+            </div>
+                      <label>Instructor?:&nbsp;
+        <input
+                        // checked={values.role}
+                        // onChange={onCheckboxChange}
+                        name='role'
+                        type='checkbox'
+                    /></label>
+
+            <button>Confirm</button>
+        </Div>
     )
 }
 
-export default InstructorRegister
+export default Register
