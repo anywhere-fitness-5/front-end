23f309c35b8f325e5182d28b747a881796ba926a
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-29 09:04:12 -0700

    updated form styling from back-up

--- my-app/src/components/UserLogin.js
+++ my-app/src/components/UserLogin.js
@@ -1,43 +1,166 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Div from './styled-comp/login-comp.jsx'
+import axios from 'axios'
+import * as yup from 'yup'
+
+const url = "https://fitness-demo.herokuapp.com/api/login"
 
 const initialFormValues = {
     username: '',
     password: '',
+    instructor: false,
 }
+const initialFormErrors = {
+    username: '',
+    password: '',
+}
+const formSchema = yup.object().shape({
+    username: yup
+        .string()
+        .min(2, '*username must have at least 2 characters!*')
+        .required('username is required'),
+    password: yup
+        .string()
+        .min(6, '*password must have at least 6 characters!*')
+        .required('password is required'),
+})
+function Login(props) {
+    const [users, setUsers] = useState([])
+    const [userValues, setUserValues] = useState(initialFormValues)
+    const [formDisabled, setFormDisabled] = useState(true)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
 
+    const postUser = user => {
+        axios.post(url, user)
+            .then(res => {
+                setUsers([...users, res.data])
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+                debugger
+            })
+    }
 
-function UserLogin(props) {
+    useEffect(() => {
+        formSchema.isValid(userValues)
+            .then(valid => {
+                setFormDisabled(!valid)
+            })
+    }, [userValues])
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+
+        const newUser = {
+            username: userValues.username,
+            password: userValues.password,
+            instructor: Object.keys(userValues.instructor),
+        }
+        postUser(newUser)
+        setUserValues(initialFormValues)
+    }
+    const onInputChange = evt => {
+        const name = evt.target.name
+        const value = evt.target.value
+        const checked = evt.target.checked
+
+        yup
+            .reach(formSchema, name)
+            .validate(value)
+            .then(valid => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: '',
+                })
+            })
+            .catch(err => {
+                setFormErrors({
+                    ...formErrors,
+                    [name]: err.errors[0]
+                })
+            })
+
+        setUserValues({
+            ...userValues,
+            [name]: value,
+        })
+    }
+    const onCheckboxChange = evt => {
+        const { name } = evt.target
+        const isChecked = evt.target.checked
+
+        setUserValues({
+            ...userValues,
+            instructor: {
+                ...userValues.instructor,
+                [name]: isChecked,
+            }
+        })
+    }
+
+    return <div>
+        <LoginForm
+            values={userValues}
+            onInputChange={onInputChange}
+            onCheckboxChange={onCheckboxChange}
+            onSubmit={onSubmit}
+            disabled={formDisabled}
+            errors={formErrors}
+        />
+    </div>
+}
+function LoginForm(props) {
+
+    const {
+        values,
+        onInputChange,
+        onCheckboxChange,
+        onSubmit,
+        disabled,
+        errors,
+    } = props
+
     return (
         <Div>
             <header>
                 <h2>Login</h2>
             </header>
             <img src="/img/swimming.jpg" className="loginImage" alt="people swimming in lanes of a pool" />
-            <label>Username:&nbsp;
-                        <input
-                    // value={values.username}
-                    // onChange={onInputChange}
-                    name='username'
-                    type='text'
-                /></label>
-            <label>Password:&nbsp;
-                        <input
-                    // value={values.password}
-                    // onChange={onInputChange}
-                    name='password'
-                    type='password'
-                /></label>
-            <label>Instructor:&nbsp;
-        <input
-                    // checked={values.role}
-                    // onChange={onCheckboxChange}
-                    name='role'
-                    type='checkbox'
-                /></label>
+            <div className="login-fields">
+                <div className='errors'>
+                    {errors.username}
+                </div>
+                <div className="login-input-form">
+                    <label>Username:&nbsp;
+                                <input
+                            value={values.username}
+                            onChange={onInputChange}
+                            name='username'
+                            type='text'
+                        /></label>
+                    <label>Password:&nbsp;
+                                <input
+                            value={values.password}
+                            onChange={onInputChange}
+                            name='password'
+                            type='password'
+                        /></label>
+                    <label>Instructor:&nbsp;
+            <input
+                            checked={values.role}
+                            onChange={onCheckboxChange}
+                            name='role'
+                            type='checkbox'
+                        /></label>
+                </div> {/* login-input-form */}
+                <div className='errors'>
+                    {errors.password}
+                </div>
+            </div> {/* login-fields */}
+            <button onClick={onSubmit} disabled={disabled}>Let's get moving!</button>
 
-            <button>Let's get moving!</button>
         </Div>
     )
 }
-export default UserLogin 
+export default Login 
--- my-app/src/components/styled-comp/login-comp.jsx
+++ my-app/src/components/styled-comp/login-comp.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Div = styled.div`
     display:flex;
     flex-direction: column;
-    height:400px;
+    height:445px;
     width: 500px;
     margin: 2% auto;
     padding: 1%;
@@ -17,21 +17,32 @@ const Div = styled.div`
         justify-content:center;
         width:100%;
     }
-    .input-form{
-        width:50%;
-        display:flex;
-        flex-direction:column;
-        justify-items:space-between;
-    }
     img{
         border: 1px solid orange;
         width:98%;
     }
+    .login-fields{
+        display:flex;
+        .errors{
+            width:20%;
+            color: crimson;
+            border: 1px solid orange;
+            padding: 1%;
+            background-color: lightgray;
+        }
+        .login-input-form{
+            width:35%;
+            display:flex;
+            flex-wrap: wrap;
+            justify-items:space-between;
+            // margin-right:28%;
+        }
+    }
+    
     button{
         width:125px;
         background-color: lightgray;
         border-radius: 15px;
     }
 `
-
 export default Div
